4bbfc90d7bbc65221af3a9343847333f6ae12669
mravi <manjhunath.ravi@colorado.edu>
2015-04-26 17:13:19 -0600

    added rotation method

--- image_preprocess.py
+++ image_preprocess.py
@@ -1,6 +1,6 @@
 import scipy as sp
 import scipy.misc as misc
-import os
+import os, re
 from skimage import io
 
 ''' class to pre process images'''
@@ -29,7 +29,33 @@ class Image_Preprocess(object):
                 image_resized = misc.imresize(image_nparray,resize_shape)
                 misc.imsave(self.dir_name_resized+'/'+folder+'/'+image, image_resized)
                 
+    def image_rotate(self, rotation_angles = (90, 180, 270)):
+        '''method to rotate images'''
+        if not os.path.exists(self.dir_name_resized):
+            print "Error: Resize Images and Try rotating on top of it"
+        
+        all_folders = list(os.walk(self.dir_name_resized))
+        
+        for folder in all_folders[0][1]:
+            print "rotating images in :", folder
+  
+            all_images = list(os.walk(self.dir_name_resized + '/'+folder))
+            for image in all_images[0][2]:
+                if image == '.DS_Store':
+                    continue
+                image_nparray = io.imread(self.dir_name_resized + '/'+folder+'/'+image)
+                for angle in rotation_angles:
+                    image_rotated = sp.ndimage.interpolation.rotate(image_nparray,angle, mode = 'nearest')
+                    image_rotated_name = re.sub('.jpg', '_' + str(angle) + '.jpg', image)
+                    misc.imsave(self.dir_name_resized +'/'+folder+'/'+image_rotated_name, image_rotated)
+                
+            
+      
+  
+            
+                
 
 if __name__ == "__main__":
     ip = Image_Preprocess()
-    ip.image_resize()
+    #ip.image_resize()
+    ip.image_rotate()
